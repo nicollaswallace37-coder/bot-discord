8cca5a25ee0b233759055a6be1330e2749d6fb5e
nicollaswallace37-coder <nicollaswallace37@gmail.com>
2026-02-18 13:16:00 -0300

    bot discord

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 const { Client, GatewayIntentBits } = require("discord.js");
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Bot está online!");
+});
+
+app.listen(PORT, () => {
+  console.log("Servidor web rodando");
+});
 
 const client = new Client({
   intents: [GatewayIntentBits.Guilds]
 });
 
-client.once("ready", function () {
+client.once("ready", () => {
   console.log("Bot online");
 });
 
-client.on("interactionCreate", async function (interaction) {
+client.on("interactionCreate", async (interaction) => {
   if (!interaction.isChatInputCommand()) return;
 
   if (interaction.commandName === "teste") {
