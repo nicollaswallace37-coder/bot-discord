9df5f485e2a94dfa7b52103b5d4854ad9cb1b5f5
nicollaswallace37-coder <nicollaswallace37@gmail.com>
2026-02-20 17:02:09 -0300

    cria sala rush

--- index.js
+++ index.js
@@ -314,3 +314,14 @@ client.on("interactionCreate", async interaction => {
 })
 
 client.login(TOKEN)
+const express = require("express")
+const app = express()
+
+app.get("/", (req, res) => {
+  res.send("Bot online.")
+})
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+  console.log("Web server ativo na porta " + PORT)
+})
